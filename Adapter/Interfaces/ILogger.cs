bac47b38b0ce2713545398558ab7cdb3647a2381
Rafael Cardoso <rafael_eng31@outlook.com>
2020-02-18 23:59:28 -0300

    Criar classes com funcionalidades defaults do sistema, ou seja, a Interface ILogger é a interface default que o sistema irá utilizar e a Classe Logger será a classe default que irá utilizar essa interface.

---
+++ Adapter/Interfaces/ILogger.cs
@@ -0,0 +1,7 @@
+﻿namespace Adapter.Interfaces
+{
+    public interface ILogger
+    {
+        void Send();
+    }
+}
---
+++ Adapter/Services/Logger.cs
@@ -0,0 +1,13 @@
+﻿using Adapter.Interfaces;
+using System;
+
+namespace Adapter.Services
+{
+    public class Logger : ILogger
+    {
+        public void Send()
+        {
+            Console.WriteLine("Send email pattern");
+        }
+    }
+}
